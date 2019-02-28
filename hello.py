eafb78decd45d8f21906f9e0fdf4976e005e43ae
Martin Dreher <luma.inc@gmail.com>
2019-02-28 12:18:06 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a - b
+
+def main():
+    print('2 + 6 =', add(2, 6))
+
+if __name__ == '__main__':
+    main()
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+import hello
+
+def test_add():
+    assert 5 + 5 == hello.add(5, 5)
